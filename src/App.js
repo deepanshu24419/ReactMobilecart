bf0ce08233e280513b4c97ce55c7350d3934239a
deepanshu <tyagideepanshu24419@gmail.com>
2020-05-20 01:08:12 +0530

    cart for mobile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Switch,Route} from 'react-router-dom';
+
 import './App.css';
+import  'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './component/Navbar';
+import ProductList from './component/ProductList';
+
+import Details from './component/Details';
+import Cart from './component/cart/Cart';
+import Default  from './component/Default';
+import Modal  from './component/Modal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Navbar />
+  <Switch >
+  <Route exact path="/" component={ProductList} ></Route>
+    <Route path="/details" component={Details} ></Route>
+    <Route path="/cart" component={Cart} ></Route>
+    <Route  component={Default} ></Route>
+  </Switch>
+  <Modal />
+ 
+  
+  </>
   );
 }
 
